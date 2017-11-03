6e5a5a99615482236c1dd2f7c32d797154b03c60
emilianolangella <emiliano.langella@gmail.com>
2017-11-03 15:10:01 +0100

    add ex7 8 9 10 11

---
+++ ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in." #tabbed line
+persian_cat = "I'm split\non a line." #double line
+backslash_cat = "I'm \\ a \\ cat." #backslah
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+""" #tab catnip a capo tab grass
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
